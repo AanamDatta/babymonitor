4dfb25125e7d645cb07be2d65c83523e04444a74
AanamDatta <78989806+AanamDatta@users.noreply.github.com>
2022-11-18 19:40:28 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+status =""
+objects = []
+function preload(){
+  
+}
+
+function setup(){
+    canvas = createCanvas(650,420)
+    canvas.center()
+    video = createCapture(VIDEO)
+    video.hide()
+    ObjectDetector = ml5.objectDetector("cocossd", modelLoaded)
+    document.getElementById("status").innerHTML = "status: detecting objects"
+
+
+}
+
+ 
+function modelLoaded(){
+    console.log("model is loaded")
+    status = true
+
+
+}
+function gotresult(error,results) {
+if (error) {
+    console.log(error)
+} else {
+    console.log(results)
+    objects = results
+}
+}
+function draw(){
+    image(video, 0,0,650,420)
+   
+    if (status != "") {
+        ObjectDetector.detect(video, gotresult )
+ 
+    for (var i = 0; i < objects.length; i++) { 
+         document.getElementById("status").innerHTML = "status: baby is detected"
+
+        var percent = floor(objects[i].confidence * 100)
+        fill("red")
+        text(objects[i].label + percent + "%", objects[i].x, objects[i].y)
+        noFill()
+        stroke("red")
+        rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height)
+        document.getElementById("number").innerHTML = "number of objects detected = " + objects.length
+      
+    }
+}
+    
+} 
